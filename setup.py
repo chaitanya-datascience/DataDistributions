6a7e67d3b0d6eb690721f4857251a82bb24a6b20
CJ <mechaitanyajoshi@gmail.com>
2020-03-28 13:55:43 -0500

    Uploading all files

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 18:49:26 2019
+
+@author: CJ
+"""
+
+from setuptools import setup
+setup(name='distributions', 
+      version='0.1',
+      description='Gaussian Distribution',
+      packages=['distributions'],
+      zip_safe=False)
